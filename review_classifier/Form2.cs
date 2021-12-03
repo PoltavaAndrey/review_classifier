10426143e0e70d35ea113a763a8bdad9fcb75c42
Андрій Полтава <poltava@dlit.dp.ua>
2021-12-03 12:59:52 +0200

    partial class

---
+++ review_classifier/Form2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace review_classifier
+{
+    public partial class Form1 : Form
+    {
+        
+    }
+}
