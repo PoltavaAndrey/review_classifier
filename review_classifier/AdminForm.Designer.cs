ab4badb8488507c07f310742265f71adbd969ce5
Андрій Полтава <poltava@dlit.dp.ua>
2021-12-09 18:17:16 +0200

    a

--- review_classifier/AdminForm.Designer.cs
+++ review_classifier/AdminForm.Designer.cs
@@ -53,6 +53,7 @@ namespace review_classifier
             this.button2.TabIndex = 1;
             this.button2.Text = "send";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
--- review_classifier/AdminForm.cs
+++ review_classifier/AdminForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,29 @@ namespace review_classifier
         public AdminForm()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+
+            string curDir = Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = Directory.GetParent(curDir);
+            DirectoryInfo directoryInfo2 = Directory.GetParent(directoryInfo.FullName);
+            start.FileName = directoryInfo2.FullName + @"\analytics\venv\Scripts\python.exe";
+            string path = directoryInfo2.FullName + @"\analytics\api.py";
+
+            start.Arguments = string.Format("{0} -c \"{1}\"", path, "worst app!");
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    MessageBox.Show(result);
+                }
+            }
         }
     }
 }
